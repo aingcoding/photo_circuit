c13ff817df264179b7df2f3933752dab4f53497a
unknown <aingcoding@gmail.com>
2026-01-26 20:18:16 +0700

    เพิ่ม comp value

--- create_connection_equation/create_equation.py
+++ create_connection_equation/create_equation.py
@@ -5,11 +5,12 @@ circuit_netlist = []
 for item in temporary:
     # 1. สร้าง Dictionary สำหรับอุปกรณ์ตัวนี้ตัวเดียว
     component_data = {}
-    component_data["comp_type_3"] = item[0]
+    component_data["comp_type"] = item[0]
     component_data["comp_name"] = item[1]
-    component_data["comp_unit"] = item[2]
-    component_data["comp_conn1"] = item[3]
-    component_data["comp_conn2"] = item[4]
+    component_data["comp_value"] = item[2]
+    component_data["comp_unit"] = item[3]
+    component_data["comp_conn1"] = item[4]
+    component_data["comp_conn2"] = item[5]
     
     # 2. ยัด Dictionary ก้อนนี้ใส่เข้าไปใน List หลัก
     circuit_netlist.append(component_data)
